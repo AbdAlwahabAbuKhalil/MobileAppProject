462ae3b7e8386c24fd6da5bfd6642fe92b48fbb5
sami-hamad <sami.mohd.hamad@gmail.com>
2023-05-30 20:54:28 +0300

    Version 5: Full Program working, but without notification

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -41,49 +41,81 @@ class Login : DialogFragment(R.layout.login) {
         val add:Button=view.findViewById(R.id.insertbtn)
         add.setOnClickListener(){
 
-            //onClickAddName()
-            val contentResolver = requireContext().contentResolver
+                val flags = BooleanArray(3) { true }
 
 
-            val values = ContentValues()
-            values.put(Book.NAME,name.text.toString() )
-            values.put(Book.PAGES, pages.text.toString().toInt())
-            values.put(Book.AUTHOR, author.text.toString())
-            values.put(Book.PRICE, price.text.toString().toInt())
-            values.put(Book.GENRE, genre.text.toString())
+            //author input validation
+                val authorText = author.text.toString()
+                val authorContainsOnlyLetters = authorText.all { it.isLetter() || it.isWhitespace()}
 
-            val uri = contentResolver.insert(Book.CONTENT_URI, values)
-            Toast.makeText(requireContext(), "Added Successfully", Toast.LENGTH_LONG).show()
-
-
-            //to add an alarm
-            val h: EditText = view.findViewById(R.id.hoursnum)
-            val m: EditText = view.findViewById(R.id.minutesnum)
-            var hour: Int = h.text.toString().toIntOrNull() ?: -1
-            var minutes: Int = m.text.toString().toIntOrNull() ?: -1
-
-            //this is to check if the alarm hour value is valid (0-23)
-            if (hour < 0 || hour > 23) {
-                h.backgroundTintList = ColorStateList.valueOf(Color.RED)
-            } else {
-                h.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
-            }
-            //this is to chheck if the alarm minute value is valid (0-59)
-            if (minutes < 0 || minutes > 59) {
-                m.backgroundTintList = ColorStateList.valueOf(Color.RED)
-            } else {
-                m.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
-            }
-            //both should be valid
-            if (hour in 0..23 && minutes in 0..59) {
-                val mess: String = "READ THE BOOK: " + name.text.toString() + " !!"
-                createAlarm(mess, hour, minutes)
-            }
+                if (authorContainsOnlyLetters) {
+                    author.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
+                } else {
+                    author.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                    flags[0] = false
+                }
 
 
-            //going back to main page after adding the book
-            dismiss() // Dismiss the dialog
-            requireActivity().supportFragmentManager.popBackStack() // Pop the fragment from the back stack
+            //genre input validation
+                val genreText = genre.text.toString()
+                val genreContainsOnlyLetters = genreText.all { it.isLetter() || it.isWhitespace()}
+
+                if (genreContainsOnlyLetters) {
+                    genre.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
+                } else {
+                    genre.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                    flags[1] = false
+                }
+
+            //to getting alarm values
+                val h: EditText = view.findViewById(R.id.hoursnum)
+                val m: EditText = view.findViewById(R.id.minutesnum)
+                var hour: Int = h.text.toString().toIntOrNull() ?: -1
+                var minutes: Int = m.text.toString().toIntOrNull() ?: -1
+
+            //alarm values validation
+                //this is to check if the alarm hour value is valid (0-23)
+                    if (hour < 0 || hour > 23) {
+                        h.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                        flags[2]=false
+                    } else {
+                        h.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
+                    }
+                    //this is to chheck if the alarm minute value is valid (0-59)
+                    if (minutes < 0 || minutes > 59) {
+                        m.backgroundTintList = ColorStateList.valueOf(Color.RED)
+                        flags[2]=false
+                    } else {
+                        m.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
+                    }
+
+            //if all input values are valid
+                if (!flags.contains(false) && hour in 0..23 && minutes in 0..59) {
+
+                    //creating the alaram
+                    val mess: String = "READ THE BOOK: " + name.text.toString() + " !!"
+                    createAlarm(mess, hour, minutes)
+
+                    //onClickAddName()
+                    val contentResolver = requireContext().contentResolver
+
+
+                    val values = ContentValues()
+                    values.put(Book.NAME,name.text.toString() )
+                    values.put(Book.PAGES, pages.text.toString().toInt())
+                    values.put(Book.AUTHOR, author.text.toString())
+                    values.put(Book.PRICE, price.text.toString().toDouble())
+                    values.put(Book.GENRE, genre.text.toString())
+
+                    val uri = contentResolver.insert(Book.CONTENT_URI, values)
+                    Toast.makeText(requireContext(), "Added Successfully", Toast.LENGTH_LONG).show()
+
+
+                    //going back to main page after adding the book
+                    dismiss() // Dismiss the dialog
+                    requireActivity().supportFragmentManager.popBackStack() // Pop the fragment from the back stack
+                }
+
 
         }
     }
